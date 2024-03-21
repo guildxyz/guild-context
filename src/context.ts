a4b6f972c5e2a19f31bce8af738161c0f84539e8
Devidxyz <devid@zgen.hu>
2024-03-21 09:33:44 +0100

    Extending context

--- src/context.ts
+++ src/context.ts
@@ -3,12 +3,12 @@ import { Meta, NoParamNoReturnFunc } from "./types";
 
 const asyncLocalStorage = new AsyncLocalStorage<Meta>();
 
-const withContext = (context: Meta, func: NoParamNoReturnFunc): void => {
-  asyncLocalStorage.run(context, () => func());
-};
-
 const getContext = (): Meta => {
   return asyncLocalStorage.getStore() || {};
+};
+
+const withContext = (context: Meta, func: NoParamNoReturnFunc): void => {
+  asyncLocalStorage.run({ ...getContext(), ...context }, () => func());
 };
 
 export default { with: withContext, get: getContext };
